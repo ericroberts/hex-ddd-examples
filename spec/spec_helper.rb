1e2582d6e559dd1b13362fff2a426e684216b84c
Eric Roberts <ericroberts@gmail.com>
2014-05-13 08:22:31 -0400

    Cleanup spec_helper for rubocop.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,8 @@
-ENV["RAILS_ENV"] ||= 'test'
-require File.expand_path("../../config/environment", __FILE__)
+ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path('../../config/environment', __FILE__)
 require 'rspec/rails'
 
-Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
 ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
 
@@ -11,5 +11,5 @@ RSpec.configure do |config|
 
   config.use_transactional_fixtures = true
 
-  config.order = "random"
+  config.order = 'random'
 end
