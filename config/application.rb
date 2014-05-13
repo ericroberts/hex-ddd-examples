0b55aed8f3e5f85e822698602916fc954f10a8c4
Eric Roberts <ericroberts@gmail.com>
2014-05-13 08:17:41 -0400

    Add services folder to autoload path.

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,6 @@ module HexDddExamples
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-    config.autoload_paths += %W(#{config.root}/forms)
+    config.autoload_paths += %W(#{config.root}/forms #{config.root}/services)
   end
 end
