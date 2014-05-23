44d36baca818d785580291f1b4d2b046c69a17c9
Eric Roberts <ericroberts@gmail.com>
2014-05-22 20:51:39 -0400

    Hook up to stripe.

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,1 +1,6 @@
-Stripe.api_key = ENV['STRIPE_SECRET']
+Rails.configuration.stripe = {
+  :publishable_key => ENV['STRIPE_PUBLIC'],
+  :secret_key      => ENV['STRIPE_SECRET']
+}
+
+Stripe.api_key = Rails.configuration.stripe[:secret_key]
